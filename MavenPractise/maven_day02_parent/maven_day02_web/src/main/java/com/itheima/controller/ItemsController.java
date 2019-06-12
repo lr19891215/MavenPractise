121780c80a3b3e8a1fed270e44460a4b2ff218ce
admin <admin@163.com>
2019-06-12 20:54:31 +0800

    maven 第五次修改

--- MavenPractise/maven_day02_parent/maven_day02_web/src/main/java/com/itheima/controller/ItemsController.java
+++ MavenPractise/maven_day02_parent/maven_day02_web/src/main/java/com/itheima/controller/ItemsController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/items")
 public class ItemsController {
 
     @Autowired
